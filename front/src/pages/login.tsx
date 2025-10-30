47c3a846da56f047ba1cfb073fe30d1d5ed9995b
sebast825 <sebamolina825@gmail.com>
2025-10-30 14:26:01 -0300

    login
    added boostrap
    created login page

---
+++ front/src/pages/login.tsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { Button, Col, Form, Row } from "react-bootstrap";
+
+function Login() {
+
+   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  function handleSubmit(e: React.FormEvent<HTMLButtonElement>) {
+   e.preventDefault();
+   console.log("Email:", email);
+   console.log("Password:", password);
+  }
+   return (
+   
+   <>
+   
+     <Row className=" justify-content-center align-items-center vh-100 vw-100 ">
+          <Col  md={4} className="p-4 border rounded-3 shadow-lg">
+            <h2 className="text-center mb-2 text-primary">Iniciar Sesión</h2>
+            <Form onSubmit={()=>{}} className="d-flex flex-column ">
+              <Form.Group controlId="formBasicNombre">
+                <Form.Label className="fw-bold"></Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Ingresar usuario"
+                 onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                  className="p-2"
+                />
+              </Form.Group>
+
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label className=""></Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Ingresar contraseña"
+           onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                  className="p-2"
+                />
+              </Form.Group>
+              <div className="mt-4 d-flex flex-column">
+                <Button className="" onClick={(e)=>handleSubmit(e)} variant="primary" type="submit">
+                  Iniciar Sesión
+                </Button>
+              </div>
+            </Form>
+            <div className="mt-2 d-flex flex-column border-top pt-3">
+              <Button
+                onClick={()=>{}}
+                variant="secondary"
+                type="submit"
+              >
+                Crear Usuario
+              </Button>
+            </div>
+       
+          </Col>
+        </Row>
+       </>
+   );
+}
+
+export default Login;
