e5c4d224823c82fd465d11da14c4d4bcc9a0f389
sebast825 <sebamolina825@gmail.com>
2025-10-30 14:38:42 -0300

    added cors

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,13 +1,18 @@
-console.log("Server is running...");
-
-
 const express = require('express');
 const healthRoutes = require('./routes/healthRoutes');
 const authRoutes = require('./routes/authRoutes');
+import cors from 'cors';
 
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:5173', 
+  credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization']
+
+}));
+
 
 app.use('/api', healthRoutes);
 app.use('/api', authRoutes);
