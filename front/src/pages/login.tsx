03fa84919d234e89bbf23826e586ef50f889ac36
sebast825 <sebamolina825@gmail.com>
2025-10-30 17:08:59 -0300

    navigate
    added navigate after login, redirect to dashboard

--- front/src/pages/login.tsx
+++ front/src/pages/login.tsx
@@ -1,66 +1,65 @@
 import { useState } from "react";
 import { Button, Col, Form, Row } from "react-bootstrap";
 import { useAuth } from "../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
-  const {login}= useAuth();
-
-   const [email, setEmail] = useState("");
+  const { login } = useAuth();
+  const navigate = useNavigate();
+  navigate("/dashboard");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  async function  handleSubmit(e: React.FormEvent<HTMLButtonElement>) {
-   e.preventDefault();
-       await login("jua2an@email.com", "miPaassword123");
-
+  async function handleSubmit(e: React.FormEvent<HTMLButtonElement>) {
+    e.preventDefault();
+    await login("jua2an@email.com", "miPaassword123");
   }
-   return (
-   
-   <>
-   
-     <Row className=" justify-content-center align-items-center vh-100 vw-100 ">
-          <Col  md={4} className="p-4 border rounded-3 shadow-lg">
-            <h2 className="text-center mb-2 text-primary">Iniciar Sesión</h2>
-            <Form onSubmit={()=>{}} className="d-flex flex-column ">
-              <Form.Group controlId="formBasicNombre">
-                <Form.Label className="fw-bold"></Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Ingresar usuario"
-                 onChange={(e) => setEmail(e.target.value)}
+  return (
+    <>
+      <Row className=" justify-content-center align-items-center vh-100 vw-100 ">
+        <Col md={4} className="p-4 border rounded-3 shadow-lg">
+          <h2 className="text-center mb-2 text-primary">Iniciar Sesión</h2>
+          <Form onSubmit={() => {}} className="d-flex flex-column ">
+            <Form.Group controlId="formBasicNombre">
+              <Form.Label className="fw-bold"></Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Ingresar usuario"
+                onChange={(e) => setEmail(e.target.value)}
                 value={email}
-                  className="p-2"
-                />
-              </Form.Group>
+                className="p-2"
+              />
+            </Form.Group>
 
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label className=""></Form.Label>
-                <Form.Control
-                  type="password"
-                  placeholder="Ingresar contraseña"
-           onChange={(e) => setPassword(e.target.value)}
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label className=""></Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Ingresar contraseña"
+                onChange={(e) => setPassword(e.target.value)}
                 value={password}
-                  className="p-2"
-                />
-              </Form.Group>
-              <div className="mt-4 d-flex flex-column">
-                <Button className="" onClick={(e)=>handleSubmit(e)} variant="primary" type="submit">
-                  Iniciar Sesión
-                </Button>
-              </div>
-            </Form>
-            <div className="mt-2 d-flex flex-column border-top pt-3">
+                className="p-2"
+              />
+            </Form.Group>
+            <div className="mt-4 d-flex flex-column">
               <Button
-                onClick={()=>{}}
-                variant="secondary"
+                className=""
+                onClick={(e) => handleSubmit(e)}
+                variant="primary"
                 type="submit"
               >
-                Crear Usuario
+                Iniciar Sesión
               </Button>
             </div>
-       
-          </Col>
-        </Row>
-       </>
-   );
+          </Form>
+          <div className="mt-2 d-flex flex-column border-top pt-3">
+            <Button onClick={() => {}} variant="secondary" type="submit">
+              Crear Usuario
+            </Button>
+          </div>
+        </Col>
+      </Row>
+    </>
+  );
 }
 
 export default Login;
