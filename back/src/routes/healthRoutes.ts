6567adc6c645b24f19256c624b3a7fa60d68eac9
sebast825 <sebamolina825@gmail.com>
2025-10-30 13:03:32 -0300

    health routes
    added health routes for api and db
    configure the port is listening

---
+++ back/src/routes/healthRoutes.ts
@@ -0,0 +1,35 @@
+import { Request, Response, Router } from 'express';
+const { connectDB } = require('../config/database.ts');
+
+const router = Router();
+
+router.get('/health-database', async (req: Request, res: Response) => {
+  try {
+    const pool = await connectDB();
+ 
+    res.json({ 
+      status: 'OK', 
+      message: 'API and DB connected successfully',
+      timestamp: new Date().toISOString(),
+      database: 'connected'
+    });
+  } catch (error) {
+    res.status(500).json({ 
+      status: 'ERROR', 
+      message: 'Error connecting to the database',
+      timestamp: new Date().toISOString(),
+      database: 'disconnected'
+    });
+  }
+});
+
+
+router.get('/health',(req : Request, res: Response) => {
+  res.json({ 
+    status: 'OK', 
+    message: 'API working correctly',
+    timestamp: new Date().toISOString()
+  });
+});
+
+module.exports = router; 
--- back/src/server.ts
+++ back/src/server.ts
@@ -1,1 +1,17 @@
 console.log("Server is running...");
+
+
+const express = require('express');
+const healthRoutes = require('./routes/healthRoutes');
+
+const app = express();
+app.use(express.json());
+
+app.use('/api', healthRoutes);
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
