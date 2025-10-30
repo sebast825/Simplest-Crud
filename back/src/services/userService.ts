845c7bb1ee4c6ed140f47589b1cc13bd9ed15a4f
sebast825 <sebamolina825@gmail.com>
2025-10-30 19:41:05 -0300

    validate email
    valide email doesn't exist before create user

--- back/src/services/userService.ts
+++ back/src/services/userService.ts
@@ -5,15 +5,20 @@ import { comparePassword, hashPassword } from "../helpers/hashPasswords";
 
 const { connectDB, sql } = require("../config/database.ts");
 
-
 class UserService {
-
-   public async create(userDto :UserCreateRequestDto) : Promise<UserResponseDto> {
-
-      try {
+  public async create(userDto: UserCreateRequestDto): Promise<UserResponseDto> {
+    try {
       const pool = await connectDB();
       const request = pool.request();
-      var hashPass :string = await hashPassword(userDto.password);
+
+      const checkEmail = await request
+        .input("email", sql.NVarChar, userDto.email)
+        .query("SELECT id FROM users WHERE email = @email");
+
+      if (checkEmail.recordset.length > 0) {
+        throw new Error("El email ya está registrado");
+      }
+      var hashPass: string = await hashPassword(userDto.password);
       const result = await request
         .input("name", sql.NVarChar, userDto.name)
         .input("email", sql.NVarChar, userDto.email)
@@ -24,52 +29,47 @@ class UserService {
       `);
       const dbUser = result.recordset[0];
 
-      const userResponse : UserResponseDto ={
+      const userResponse: UserResponseDto = {
         name: dbUser.name,
-        id: dbUser.id
-      }
+        id: dbUser.id,
+      };
       return userResponse;
     } catch (error: any) {
       // ✅ Aquí capturas errores de SQL
       console.error("Error DB:", error.message);
       throw error; // Re-lanzas para que el controller lo maneje
     }
-   }
-   public async emailPasswordMatch(loginDto :AuthRequestDto) : Promise<UserResponseDto> {
-
-      try {
-    const pool = await connectDB();
-  const request = pool.request();
-  const result = await request
-    .input("email", sql.NVarChar, loginDto.email)
-    .query(`
+  }
+  public async emailPasswordMatch(
+    loginDto: AuthRequestDto
+  ): Promise<UserResponseDto> {
+    try {
+      const pool = await connectDB();
+      const request = pool.request();
+      const result = await request.input("email", sql.NVarChar, loginDto.email)
+        .query(`
       SELECT id, password,name
       FROM users
       WHERE email = @email 
     `);
 
       const dbUser = result.recordset[0];
-    
-    if(await comparePassword(loginDto.password, dbUser.password)){
-         const userResponse : UserResponseDto ={
-        name: dbUser.name,
-        id: dbUser.id
+
+      if (await comparePassword(loginDto.password, dbUser.password)) {
+        const userResponse: UserResponseDto = {
+          name: dbUser.name,
+          id: dbUser.id,
+        };
+        return userResponse;
+      } else {
+        throw new Error("Invalid email or password");
       }
-    return userResponse;
-    
-
-    }else{
-    throw new Error("Invalid email or password");
-
-    }
-
     } catch (error: any) {
       // ✅ Aquí capturas errores de SQL
       console.error("Error DB:", error.message);
       throw error; // Re-lanzas para que el controller lo maneje
     }
-   }
-
+  }
 }
 
 export default UserService;
