6bc0c37c0cb8688dc5a9e340c902dc89e02d4dd9
sebast825 <sebamolina825@gmail.com>
2025-10-30 12:53:57 -0300

    db config
    added env variables
    created .env.Example (create a file .env with valid credentials)

---
+++ back/src/config/database.ts
@@ -0,0 +1,28 @@
+import sql from 'mssql';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const dbConfig = {
+    server: process.env.DB_SERVER ?? 'localhost',
+    port: Number(process.env.DB_PORT),
+    database: process.env.DB_DATABASE ,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    options: {
+        encrypt: process.env.DB_ENCRYPT === 'true',
+        trustServerCertificate: process.env.DB_TRUST_CERT === 'true',
+        enableArithAbort: true,
+    }
+};
+let pool : sql.ConnectionPool| null = null;;
+
+const connectDB  = async (): Promise<sql.ConnectionPool>   => {
+    if (!pool) {
+        pool = await sql.connect(dbConfig);
+        console.log('✅ Conectado a SQL Server');
+    }
+    return pool;
+};
+
+module.exports = { connectDB, sql };
