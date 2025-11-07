39e9d31bc5a927408d1858012eb9942000537d3e
sebast825 <sebamolina825@gmail.com>
2025-11-07 19:35:45 -0300

    no tasks text
    in case no task created show a meesage
    updated padding

--- front/src/pages/dashboard.tsx
+++ front/src/pages/dashboard.tsx
@@ -12,7 +12,7 @@ function Dashboard() {
 
   return (
     <>
-      <div className="vw-100 vh-100 p-5">
+      <div className="vw-100 vh-100 p-3 p-sm-5">
         <Container
           fluid
           className="bg-primary text-white text-center py-5 rounded-3 mb-4"
@@ -20,53 +20,57 @@ function Dashboard() {
           <h1 className="fw-bold">Bienvenido {user?.name}!</h1>
         </Container>
         <Row className="d-flex justify-content-center align-items-center ">
-          <Container className="mt-4 ">
+          <Container className="mt-4  mt-ms-1">
             <Row className="mb-3 align-items-center">
               <Col>
-                <h2>Mis tareas</h2>
+                <h2 >Mis tareas</h2>
               </Col>
               <Col className="text-end">
                 <CreateTaskModal onCreate={(title) => createTask(title)} />{" "}
               </Col>
             </Row>
 
-            <ListGroup>
-              {tasks
-                .sort((a, b) => Number(a.done) - Number(b.done))
-                .map((task) => (
-                  <ListGroup.Item
-                    key={task.id}
-                    variant={task.done ? "success" : ""}
-                    className="d-flex justify-content-between align-items-center"
-                  >
-                    <div>
-                      <strong>{task.title}</strong>{" "}
-                      <span
-                        className={task.done ? "text-success" : "text-muted"}
-                      >
-                        {task.done ? "Hecha" : "Pendiente"}
-                      </span>
-                    </div>
+            <ListGroup className="bg-light text-darl text-center py-5 rounded-3 mb-4">
+              {tasks.length == 0 ? (
+                <h3>No tienes tareas!</h3>
+              ) : (
+                tasks
+                  .sort((a, b) => Number(a.done) - Number(b.done))
+                  .map((task) => (
+                    <ListGroup.Item
+                      key={task.id}
+                      variant={task.done ? "success" : ""}
+                      className="d-flex justify-content-between align-items-center"
+                    >
+                      <div>
+                        <strong>{task.title}</strong>{" "}
+                        <span
+                          className={task.done ? "text-success" : "text-muted"}
+                        >
+                          {task.done ? "Hecha" : "Pendiente"}
+                        </span>
+                      </div>
 
-                    <div className="d-flex gap-2">
-                      <Button
-                        size="sm"
-                        variant={task.done ? "warning" : "success"}
-                        onClick={() => updateTaskStatus(task.id)}
-                      >
-                        {task.done ? "Marcar pendiente" : "Marcar hecha"}
-                      </Button>
+                      <div className="d-flex gap-2">
+                        <Button
+                          size="sm"
+                          variant={task.done ? "warning" : "success"}
+                          onClick={() => updateTaskStatus(task.id)}
+                        >
+                          {task.done ? "Marcar pendiente" : "Marcar hecha"}
+                        </Button>
 
-                      <Button
-                        size="sm"
-                        variant="danger"
-                        onClick={() => deleteTask(task.id)}
-                      >
-                        Eliminar
-                      </Button>
-                    </div>
-                  </ListGroup.Item>
-                ))}
+                        <Button
+                          size="sm"
+                          variant="danger"
+                          onClick={() => deleteTask(task.id)}
+                        >
+                          Eliminar
+                        </Button>
+                      </div>
+                    </ListGroup.Item>
+                  ))
+              )}
             </ListGroup>
           </Container>
         </Row>
