10fb373088f88d5b4bd4e09d360c43d98748a4f6
sebast825 <sebamolina825@gmail.com>
2025-11-07 14:45:42 -0300

    removed try catch from service
    removed try catch from all fns, already is handled in controller

--- back/src/services/taskService.ts
+++ back/src/services/taskService.ts
@@ -5,79 +5,59 @@ import { CustomError } from "../helpers/customError";
 
 class TaskService {
   public async getTasksByUser(userId: string): Promise<void> {
-    try {
-      if (!userId) {
-        throw new CustomError("Uuser Id is required", 400);
-      }
-      const pool = await connectDB();
-
-      const result = await pool
-        .request()
-        .input("userId", sql.Int, parseInt(userId))
-        .query("SELECT * FROM tasks WHERE userId = @userId");
-
-      return result.recordset;
-    } catch (error) {
-      console.error("Error fetching tasks:", error);
-      throw error;
+    if (!userId) {
+      throw new CustomError("Uuser Id is required", 400);
     }
+    const pool = await connectDB();
+
+    const result = await pool
+      .request()
+      .input("userId", sql.Int, parseInt(userId))
+      .query("SELECT * FROM tasks WHERE userId = @userId");
+
+    return result.recordset;
   }
   public async updateTaskStatus(taskId: number) {
-    try {
-      if (!taskId) {
-        throw new CustomError("Task Id is required", 400);
-      }
-      const pool = await connectDB();
-      const result = await pool
-        .request()
-        .input("taskId", sql.Int, taskId)
-        .query(
-          "UPDATE tasks SET done = CASE WHEN done = 1 THEN 0 ELSE 1 END OUTPUT inserted.* WHERE id = @taskId"
-        );
-
-      return result.recordset[0];
-    } catch (error) {
-      console.error("Error updating task:", error);
-      throw error;
+    if (!taskId) {
+      throw new CustomError("Task Id is required", 400);
     }
+    const pool = await connectDB();
+    const result = await pool
+      .request()
+      .input("taskId", sql.Int, taskId)
+      .query(
+        "UPDATE tasks SET done = CASE WHEN done = 1 THEN 0 ELSE 1 END OUTPUT inserted.* WHERE id = @taskId"
+      );
+
+    return result.recordset[0];
   }
   public async deleteTask(taskId: number) {
-    try {
-      if (!taskId) {
-        throw new CustomError("Task Id is required", 400);
-      }
-      const pool = await connectDB();
-      const result = await pool
-        .request()
-        .input("taskId", sql.Int, taskId)
-        .query("DELETE FROM tasks OUTPUT deleted.* WHERE id = @taskId");
-
-      return result.recordset[0];
-    } catch (error) {
-      console.error("Error deleting task:", error);
-      throw error;
+    if (!taskId) {
+      throw new CustomError("Task Id is required", 400);
     }
+    const pool = await connectDB();
+    const result = await pool
+      .request()
+      .input("taskId", sql.Int, taskId)
+      .query("DELETE FROM tasks OUTPUT deleted.* WHERE id = @taskId");
+
+    return result.recordset[0];
   }
 
   public async createTask(title: string, userId: number) {
-    try {
-      if (!userId || !title) {
-        throw new CustomError("User Id and title are required", 400);
-      }
-      const pool = await connectDB();
-      const result = await pool
-        .request()
-        .input("title", sql.NVarChar, title)
-        .input("userId", sql.Int, userId)
-        .query(
-          "INSERT INTO tasks (title, done, userId) OUTPUT inserted.* VALUES (@title, 0, @userId)"
-        );
-
-      return result.recordset[0]; // devuelve la tarea recién creada
-    } catch (error) {
-      console.error("Error creating task:", error);
-      throw error;
+    if (!userId || !title) {
+      throw new CustomError("User Id and title are required", 400);
     }
+    const pool = await connectDB();
+    const result = await pool
+      .request()
+      .input("title", sql.NVarChar, title)
+      .input("userId", sql.Int, userId)
+      .query(
+        "INSERT INTO tasks (title, done, userId) OUTPUT inserted.* VALUES (@title, 0, @userId)"
+      );
+
+    return result.recordset[0]; // devuelve la tarea recién creada
   }
 }
 
