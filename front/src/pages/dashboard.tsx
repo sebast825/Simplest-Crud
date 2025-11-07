25c25d245080b30d64430273f1ebc11c4e5c7489
sebast825 <sebamolina825@gmail.com>
2025-11-07 19:26:58 -0300

    fix
    rename useRegister extension from tsx to ts
    in dashboard reorder the if return

--- front/src/pages/dashboard.tsx
+++ front/src/pages/dashboard.tsx
@@ -1,75 +1,77 @@
-import { Button,  Col, Container, ListGroup, Row } from "react-bootstrap";
+import { Button, Col, Container, ListGroup, Row } from "react-bootstrap";
 import { useTasks } from "../hooks/useTasks";
 import CreateTaskModal from "../modals/createTaskModal";
-import { useLocation } from 'react-router-dom';
+import { useLocation } from "react-router-dom";
 
 function Dashboard() {
-  const { loading, tasks, updateTaskStatus, deleteTask,createTask } = useTasks();
-  if (loading) return <p>Cargando tareas...</p>;
+  const { loading, tasks, updateTaskStatus, deleteTask, createTask } =
+    useTasks();
   const location = useLocation();
   const user = location.state?.user;
+  if (loading) return <p>Cargando tareas...</p>;
 
   return (
-   <>
-   <div className="vw-100 vh-100 p-5">
-<Container fluid className="bg-primary text-white text-center py-5 rounded-3 mb-4">
-      <h1 className="fw-bold">
-        Bienvenido {user?.name}!
-      </h1>
-    </Container>
-    <Row className="d-flex justify-content-center align-items-center ">
-      <Container className="mt-4 ">
-        <Row className="mb-3 align-items-center">
-          <Col>
-            <h2>Mis tareas</h2>
-          </Col>
-          <Col className="text-end">
-            <CreateTaskModal
-              onCreate={(title)=>createTask(title)}
-            />{" "}
-          </Col>
+    <>
+      <div className="vw-100 vh-100 p-5">
+        <Container
+          fluid
+          className="bg-primary text-white text-center py-5 rounded-3 mb-4"
+        >
+          <h1 className="fw-bold">Bienvenido {user?.name}!</h1>
+        </Container>
+        <Row className="d-flex justify-content-center align-items-center ">
+          <Container className="mt-4 ">
+            <Row className="mb-3 align-items-center">
+              <Col>
+                <h2>Mis tareas</h2>
+              </Col>
+              <Col className="text-end">
+                <CreateTaskModal onCreate={(title) => createTask(title)} />{" "}
+              </Col>
+            </Row>
+
+            <ListGroup>
+              {tasks
+                .sort((a, b) => Number(a.done) - Number(b.done))
+                .map((task) => (
+                  <ListGroup.Item
+                    key={task.id}
+                    variant={task.done ? "success" : ""}
+                    className="d-flex justify-content-between align-items-center"
+                  >
+                    <div>
+                      <strong>{task.title}</strong>{" "}
+                      <span
+                        className={task.done ? "text-success" : "text-muted"}
+                      >
+                        {task.done ? "Hecha" : "Pendiente"}
+                      </span>
+                    </div>
+
+                    <div className="d-flex gap-2">
+                      <Button
+                        size="sm"
+                        variant={task.done ? "warning" : "success"}
+                        onClick={() => updateTaskStatus(task.id)}
+                      >
+                        {task.done ? "Marcar pendiente" : "Marcar hecha"}
+                      </Button>
+
+                      <Button
+                        size="sm"
+                        variant="danger"
+                        onClick={() => deleteTask(task.id)}
+                      >
+                        Eliminar
+                      </Button>
+                    </div>
+                  </ListGroup.Item>
+                ))}
+            </ListGroup>
+          </Container>
         </Row>
-
-        <ListGroup>
-          {tasks
-           .sort((a, b) => Number(a.done) - Number(b.done))
-          .map((task) => (
-            <ListGroup.Item
-              key={task.id}
-              variant={task.done ? "success" : ""}
-              className="d-flex justify-content-between align-items-center"
-            >
-              <div>
-                <strong>{task.title}</strong>{" "}
-                <span className={task.done ? "text-success" : "text-muted"}>
-                  {task.done ? "Hecha" : "Pendiente"}
-                </span>
-              </div>
-
-              <div className="d-flex gap-2">
-                <Button
-                  size="sm"
-                  variant={task.done ? "warning" : "success"}
-                  onClick={() => updateTaskStatus(task.id)}
-                >
-                  {task.done ? "Marcar pendiente" : "Marcar hecha"}
-                </Button>
-
-                <Button
-                  size="sm"
-                  variant="danger"
-                  onClick={() => deleteTask(task.id)}
-                >
-                  Eliminar
-                </Button>
-              </div>
-            </ListGroup.Item>
-          ))}
-        </ListGroup>
-      </Container>
-    </Row>
-    </div>
-       </>
+      </div>
+    </>
   );
 }
 
